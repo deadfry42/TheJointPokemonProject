3690e3018cd5aa53b4fa25aa7bc62f91debb9be5
deadfry42 <pooface442@gmail.com>
2025-05-30 13:24:37 +0100

    add more consts, move to constants folder

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use current_platform::{COMPILED_ON, CURRENT_PLATFORM};
 mod pkmncore;
 
-use pkmncore::pokemon::*;
+use pkmncore::constants::pokemon::*;
 
 fn main() {
     println!(
@@ -9,52 +9,12 @@ fn main() {
         CURRENT_PLATFORM, COMPILED_ON
     );
 
-    let wooper = Pokemon {
-        name: "Wooper",
-        pokedex: PokedexInfo {
-            index: 0,
-            height: 0.4,
-            weight: 8.5,
-            species: "Water Fish",
-            entry: "This pokemon is gay",
-        },
-        evyeild: EVYield {
-            health: Some(1),
-            speed: None,
-            atk: None,
-            def: None,
-            spatk: None,
-            spdef: None,
-        },
-        abilities: AbilitySet {
-            ability1: Ability::Damp,
-            ability2: Some(Ability::WaterAbsorb),
-            hability: Ability::Unaware,
-        },
-        base_stats: BaseStats {
-            health: 55,
-            atk: 45,
-            def: 45,
-            spatk: 25,
-            spdef: 25,
-            speed: 15,
-        },
-        types: TypeSet {
-            type1: Type::Water,
-            type2: Some(Type::Ground),
-            egg_group1: EggGroup::Field,
-            egg_group2: Some(EggGroup::Water1),
-        },
-        levelling_curve: LevellingCurve::MediumFast,
-        catch_rate: 255,
-        base_friendship: 50,
-        egg_cycles: 20,
-        gender_ratio: Some(0.5),
-        base_exp: 42,
-    };
+    let wooper = Pokemon::Bulbasaur.get_base();
 
-    println!("{}", wooper.base_stats.atk);
+    // println!("{}", wooper.base_stats.atk);
+    // println!("{}", wooper.abilities.ability1);
+    // println!("{}", wooper.levelling_curve);
     println!("{}", wooper.summarise());
-    println!("{}", wooper.get_gender(0.5));
-    println!("{}", wooper.types.egg_group1)
+    // println!("{}", wooper.get_gender(0.5));
+    // println!("{}", wooper.types.egg_group1)
 }
--- src/pkmncore/pokemon.rs
+++ src/pkmncore/pokemon.rs
@@ -1,7 +1,7 @@
-use std::fmt::{self};
+use crate::pkmncore::constants::{abilities::*, enums::*, typing::*};
 
 #[allow(dead_code)]
-pub struct Pokemon {
+pub struct PokemonBase {
     pub name: &'static str,
     pub types: TypeSet,
     pub pokedex: PokedexInfo,
@@ -13,7 +13,75 @@ pub struct Pokemon {
     pub abilities: AbilitySet,
     pub levelling_curve: LevellingCurve,
     pub base_exp: i16,
-    pub evyeild: EVYield,
+    pub ev_yield: EVs,
+}
+
+#[allow(dead_code)]
+impl PokemonBase {
+    pub fn summarise(&self) -> String {
+        format!(
+            "Pokemon {}\nType1: {}\nPokedexInfo: {}, {}\nStats: {}hp, {}spd, {}atk, {}def, {}spatk, {}spdef\nCatch rate: {}\nFriendship: {}\nGender Ratio: {}% to be Male.\nEgg cycles: {}\nAbility1: {}",
+            self.name,
+            self.types.type1,
+            self.pokedex.species,
+            self.pokedex.entry,
+            self.base_stats.health,
+            self.base_stats.speed,
+            self.base_stats.atk,
+            self.base_stats.def,
+            self.base_stats.spatk,
+            self.base_stats.spdef,
+            self.catch_rate,
+            self.base_friendship,
+            self.gender_ratio.unwrap() * 100.0,
+            self.egg_cycles,
+            self.abilities.ability1,
+        )
+    }
+
+    pub fn get_gender(&self, gender_float: f32) -> Gender {
+        if self.gender_ratio.is_none() {
+            return Gender::Unknown;
+        } else {
+            if gender_float < self.gender_ratio.unwrap() {
+                return Gender::Male;
+            } else {
+                return Gender::Female;
+            }
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub struct OTInformation {
+    id: i32,
+    sid: i32,
+    lang: Language,
+    gender: Gender,
+    name: &'static str,
+}
+
+#[allow(dead_code)]
+pub struct PokemonData {
+    nickname: &'static str,
+    ot: OTInformation,
+    base: PokemonBase,
+    evs: EVs,
+    ivs: IVs,
+    exp: i32,
+    ability: Ability,
+    mettime: i64,
+    pid: i32,
+    isegg: bool,
+    friendship: i8,
+    pokeball: Pokeball,
+    pokerus: bool,
+    marking: Marking,
+    condition: StatusCondition,
+    // TODO:
+    // metlocation: Location
+    // helditem: Item
+    //
 }
 
 #[allow(dead_code)]
@@ -32,14 +100,7 @@ pub struct TypeSet {
 }
 
 #[allow(dead_code)]
-pub enum Ability {
-    Damp,
-    WaterAbsorb,
-    Unaware,
-}
-
-#[allow(dead_code)]
-pub struct EVYield {
+pub struct EVs {
     pub health: Option<i16>,
     pub speed: Option<i16>,
     pub atk: Option<i16>,
@@ -49,347 +110,22 @@ pub struct EVYield {
 }
 
 #[allow(dead_code)]
+pub struct IVs {
+    pub health: i8,
+    pub speed: i8,
+    pub atk: i8,
+    pub def: i8,
+    pub spatk: i8,
+    pub spdef: i8,
+}
+
+#[allow(dead_code)]
 pub struct PokedexInfo {
     pub index: i32,
     pub height: f64,
     pub weight: f64,
     pub species: &'static str,
     pub entry: &'static str,
-}
-
-// pub struct BoxPokemon {
-//     pub pkmn: Pokemon,
-// }
-
-#[allow(dead_code)]
-pub enum LevellingCurve {
-    Erratic,
-    Fast,
-    MediumFast,
-    MediumSlow,
-    Slow,
-    Fluctuating,
-}
-
-impl fmt::Display for LevellingCurve {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            LevellingCurve::Erratic => write!(f, "Erratic"),
-            LevellingCurve::Fast => write!(f, "Fast"),
-            LevellingCurve::MediumFast => write!(f, "MediumFast"),
-            LevellingCurve::MediumSlow => write!(f, "MediumSlow"),
-            LevellingCurve::Slow => write!(f, "Slow"),
-            LevellingCurve::Fluctuating => write!(f, "Fluctuating"),
-        }
-    }
-}
-
-pub enum Gender {
-    Male,
-    Female,
-    Unknown,
-}
-
-impl fmt::Display for Gender {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Gender::Male => write!(f, "Male"),
-            Gender::Female => write!(f, "Female"),
-            Gender::Unknown => write!(f, "Unknown"),
-        }
-    }
-}
-
-#[allow(dead_code)]
-pub trait Typing {
-    fn get_type_multiplier(&self, opposing_type: Type) -> f64;
-}
-
-#[allow(dead_code)]
-pub enum Type {
-    Normal,
-    Fire,
-    Water,
-    Electric,
-    Grass,
-    Ice,
-    Fighting,
-    Poison,
-    Ground,
-    Flying,
-    Psychic,
-    Bug,
-    Rock,
-    Ghost,
-    Dragon,
-    Dark,
-    Steel,
-    Fairy,
-}
-
-impl Typing for Type {
-    fn get_type_multiplier(&self, opposing_type: Type) -> f64 {
-        match self {
-            Type::Normal => match opposing_type {
-                Type::Rock => 0.5,
-                Type::Ghost => 0.0,
-                Type::Steel => 0.5,
-                _ => 1.0,
-            },
-            Type::Fire => match opposing_type {
-                Type::Fire => 0.5,
-                Type::Water => 0.5,
-                Type::Grass => 2.0,
-                Type::Ice => 2.0,
-                Type::Bug => 2.0,
-                Type::Rock => 0.5,
-                Type::Dragon => 0.5,
-                Type::Steel => 2.0,
-                _ => 1.0,
-            },
-            Type::Water => match opposing_type {
-                Type::Fire => 2.0,
-                Type::Water => 0.5,
-                Type::Grass => 0.5,
-                Type::Ground => 2.0,
-                Type::Rock => 2.0,
-                Type::Dragon => 0.5,
-                _ => 1.0,
-            },
-            Type::Electric => match opposing_type {
-                Type::Water => 2.0,
-                Type::Electric => 0.5,
-                Type::Grass => 0.5,
-                Type::Ground => 0.0,
-                Type::Flying => 2.0,
-                Type::Dragon => 0.5,
-                _ => 1.0,
-            },
-            Type::Grass => match opposing_type {
-                Type::Fire => 0.5,
-                Type::Water => 2.0,
-                Type::Grass => 0.5,
-                Type::Poison => 0.5,
-                Type::Ground => 2.0,
-                Type::Flying => 0.5,
-                Type::Bug => 0.5,
-                Type::Rock => 2.0,
-                Type::Dragon => 0.5,
-                Type::Steel => 0.5,
-                _ => 1.0,
-            },
-            Type::Ice => match opposing_type {
-                Type::Fire => 0.5,
-                Type::Water => 0.5,
-                Type::Grass => 2.0,
-                Type::Ice => 0.5,
-                Type::Ground => 2.0,
-                Type::Flying => 2.0,
-                Type::Dragon => 2.0,
-                Type::Steel => 0.5,
-                _ => 1.0,
-            },
-            Type::Fighting => match opposing_type {
-                Type::Normal => 2.0,
-                Type::Ice => 2.0,
-                Type::Poison => 0.5,
-                Type::Flying => 0.5,
-                Type::Psychic => 0.5,
-                Type::Bug => 0.5,
-                Type::Rock => 2.0,
-                Type::Ghost => 0.0,
-                Type::Dark => 2.0,
-                Type::Steel => 2.0,
-                Type::Fairy => 0.5,
-                _ => 1.0,
-            },
-            Type::Poison => match opposing_type {
-                Type::Grass => 2.0,
-                Type::Poison => 0.5,
-                Type::Ground => 0.5,
-                Type::Rock => 0.5,
-                Type::Ghost => 0.5,
-                Type::Steel => 0.0,
-                Type::Fairy => 2.0,
-                _ => 1.0,
-            },
-            Type::Ground => match opposing_type {
-                Type::Fire => 2.0,
-                Type::Electric => 2.0,
-                Type::Grass => 0.5,
-                Type::Poison => 2.0,
-                Type::Flying => 0.0,
-                Type::Bug => 0.5,
-                Type::Rock => 2.0,
-                Type::Steel => 2.0,
-                _ => 1.0,
-            },
-            Type::Flying => match opposing_type {
-                Type::Electric => 0.5,
-                Type::Grass => 2.0,
-                Type::Fighting => 2.0,
-                Type::Bug => 2.0,
-                Type::Rock => 0.5,
-                Type::Steel => 0.5,
-                _ => 1.0,
-            },
-            Type::Psychic => match opposing_type {
-                Type::Fighting => 2.0,
-                Type::Poison => 2.0,
-                Type::Psychic => 0.5,
-                Type::Dark => 0.0,
-                Type::Steel => 0.5,
-                _ => 1.0,
-            },
-            Type::Bug => match opposing_type {
-                Type::Fire => 0.5,
-                Type::Grass => 2.0,
-                Type::Fighting => 0.5,
-                Type::Poison => 0.5,
-                Type::Flying => 0.5,
-                Type::Psychic => 2.0,
-                Type::Ghost => 0.5,
-                Type::Dark => 2.0,
-                Type::Steel => 0.5,
-                Type::Fairy => 0.5,
-                _ => 1.0,
-            },
-            Type::Rock => match opposing_type {
-                Type::Fire => 2.0,
-                Type::Ice => 2.0,
-                Type::Fighting => 0.5,
-                Type::Ground => 0.5,
-                Type::Flying => 2.0,
-                Type::Bug => 2.0,
-                Type::Steel => 0.5,
-                _ => 1.0,
-            },
-            Type::Ghost => match opposing_type {
-                Type::Normal => 0.0,
-                Type::Psychic => 2.0,
-                Type::Ghost => 2.0,
-                Type::Dark => 0.5,
-                _ => 1.0,
-            },
-            Type::Dragon => match opposing_type {
-                Type::Dragon => 2.0,
-                Type::Steel => 0.5,
-                Type::Fairy => 0.0,
-                _ => 1.0,
-            },
-            Type::Dark => match opposing_type {
-                Type::Fighting => 0.5,
-                Type::Psychic => 2.0,
-                Type::Ghost => 2.0,
-                Type::Dark => 0.5,
-                Type::Fairy => 0.5,
-                _ => 1.0,
-            },
-            Type::Steel => match opposing_type {
-                Type::Fire => 0.5,
-                Type::Water => 0.5,
-                Type::Electric => 0.5,
-                Type::Ice => 2.0,
-                Type::Rock => 2.0,
-                Type::Steel => 0.5,
-                Type::Fairy => 2.0,
-                _ => 1.0,
-            },
-            Type::Fairy => match opposing_type {
-                Type::Fire => 0.5,
-                Type::Fighting => 2.0,
-                Type::Poison => 0.5,
-                Type::Dragon => 2.0,
-                Type::Dark => 2.0,
-                Type::Steel => 0.5,
-                _ => 1.0,
-            },
-        }
-    }
-}
-
-impl fmt::Display for Type {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Type::Normal => write!(f, "Normal"),
-            Type::Water => write!(f, "Water"),
-            Type::Fire => write!(f, "Fire"),
-            Type::Grass => write!(f, "Grass"),
-            Type::Psychic => write!(f, "Psychic"),
-            Type::Ground => write!(f, "Ground"),
-            Type::Rock => write!(f, "Rock"),
-            Type::Bug => write!(f, "Bug"),
-            Type::Steel => write!(f, "Steel"),
-            Type::Dark => write!(f, "Dark"),
-            Type::Ice => write!(f, "Ice"),
-            Type::Dragon => write!(f, "Dragon"),
-            Type::Fairy => write!(f, "Fairy"),
-            Type::Flying => write!(f, "Flying"),
-            Type::Ghost => write!(f, "Ghost"),
-            Type::Fighting => write!(f, "Fighting"),
-            Type::Poison => write!(f, "Poison"),
-            Type::Electric => write!(f, "Electric"),
-        }
-    }
-}
-
-#[allow(dead_code)]
-pub enum EggGroup {
-    Mineral,
-    Amorphous,
-    Grass,
-    Water1,
-    Water2,
-    Water3,
-    Bug,
-    Dragon,
-    Flying,
-    Field,
-    HumanLike,
-    Fairy,
-    Monster,
-}
-
-impl fmt::Display for EggGroup {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            EggGroup::Mineral => write!(f, "Mineral"),
-            EggGroup::Amorphous => write!(f, "Amorphous"),
-            EggGroup::Grass => write!(f, "Grass"),
-            EggGroup::Water1 => write!(f, "Water1"),
-            EggGroup::Water2 => write!(f, "Water2"),
-            EggGroup::Water3 => write!(f, "Water3"),
-            EggGroup::Bug => write!(f, "Bug"),
-            EggGroup::Dragon => write!(f, "Dragon"),
-            EggGroup::Flying => write!(f, "Flying"),
-            EggGroup::Field => write!(f, "Field"),
-            EggGroup::HumanLike => write!(f, "HumanLike"),
-            EggGroup::Fairy => write!(f, "Fairy"),
-            EggGroup::Monster => write!(f, "Monster"),
-        }
-    }
-}
-
-#[allow(dead_code)]
-impl Pokemon {
-    pub fn summarise(&self) -> String {
-        format!(
-            "Pokemon {}, {}, eggCycles: {}",
-            self.name, self.pokedex.entry, self.egg_cycles
-        )
-    }
-
-    pub fn get_gender(&self, gender_float: f32) -> Gender {
-        if self.gender_ratio.is_none() {
-            return Gender::Unknown;
-        } else {
-            if gender_float >= self.gender_ratio.unwrap() {
-                return Gender::Male;
-            } else {
-                return Gender::Female;
-            }
-        }
-    }
 }
 
 #[allow(dead_code)]
