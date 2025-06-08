e313cd52f5fa412b12beac18e24295c4fbb91625
deadfry42 <pooface442@gmail.com>
2025-06-08 16:28:43 +0100

    put back old debug prints fix item translation keys

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use current_platform::{COMPILED_ON, CURRENT_PLATFORM};
 use lazy_static::lazy_static;
 use pkmncore::constants::pokemon::*;
 use std::sync::Mutex;
 
+use crate::assetcore::assets::check_for_assets;
 use crate::assetcore::gamedata::GameData;
 use crate::i18ncore::loaded::LoadedLocales;
-use crate::pkmncore::constants::enums::Gender;
-use crate::pkmncore::constants::moves::{Move, MoveType};
-use crate::pkmncore::constants::typing::Type;
-use crate::{assetcore::assets::check_for_assets, i18ncore::keys::TranslationKey};
-use crate::{i18ncore::parsing::*, pkmncore::constants::natures::Nature};
+use crate::i18ncore::parsing::*;
+use crate::pkmncore::constants::enums::*;
+use crate::pkmncore::constants::items::*;
+use crate::pkmncore::constants::moves::MoveType;
+
+use crate::pkmncore::boxes::pc::*;
+use crate::pkmncore::rng::*;
+use crate::pkmncore::trainer::*;
+
+use crate::utils::*;
 
 extern crate lazy_static;
 
@@ -20,12 +25,6 @@ pub mod utils;
 
 #[allow(dead_code)]
 const GAME_VERSION: &str = "v0.0-beta";
-
-// pub static GAME_DATA: LazyLock<Mutex<GameData>> = LazyLock::new(|| {
-//     Mutex::new(GameData {
-//         loaded_locales: LoadedLocales::new(),
-//     })
-// });
 
 lazy_static! {
     pub static ref GAME_DATA: Mutex<GameData> = Mutex::new(GameData {
@@ -48,100 +47,95 @@ fn main() {
         .loaded_locales
         .set_selected_locale("en_lolcat");
 
-    println!(
-        "Hello, world from {}! I was compiled on {}.",
-        CURRENT_PLATFORM, COMPILED_ON
-    );
-
-    println!(
-        "Translated string (code_name): {}",
-        TranslationKey::new("code_name").convert_to_string()
-    );
-    println!(
-        "Translated string (name): {}",
-        TranslationKey::new("name").convert_to_string()
-    );
-    println!(
-        "Translated string (pokemon/bulbasaur/species): {}",
-        Pokemon::Bulbasaur.get_base().name.convert_to_string()
-    );
-    println!("Translated string (type/fire): {}", Type::Fire);
-    println!("Translated string (gender/male): {}", Gender::Male);
-    println!(
-        "Translated string (move/tackle/desc): {}",
-        Move::Tackle.get_base().desc.convert_to_string()
-    );
-    println!("Translated string (nature/quirky): {}", Nature::Quirky);
-
-    // let plr = Player {
-    //     trainer: Trainer {
-    //         info: OTInformation {
-    //             sid: generate_trainer_id(),
-    //             id: generate_trainer_id(),
-    //             lang: Language::English,
-    //             gender: Gender::Male,
-    //             name: "Yes",
-    //         },
-    //     },
-    //     pc: PC::new_dummy(),
-    //     money: 0,
-    //     party: [None, None, None, None, None, None],
-    // };
-
-    // let wooper = generate_wild_pokemon(Pokemon::Ivysaur, 69, &plr);
-
-    // println!("health evs: {}", wooper.get_ev(&Stat::Health));
-    // println!("health ivs: {}", wooper.get_iv(&Stat::Health));
-    // println!("nature: {}", wooper.nature);
-    // println!("health stat: {}", wooper.calculate_stat(&Stat::Health));
-    // println!("lvl {} ({} exp)", wooper.get_level(), wooper.exp,);
     // println!(
-    //     "Personality: 0x{} (0b{})",
-    //     hex::decimal_to_hex(wooper.pid),
-    //     hex::decimal_to_binary(wooper.pid)
+    //     "Hello, world from {}! I was compiled on {}.",
+    //     CURRENT_PLATFORM, COMPILED_ON
     // );
-    // // println!(
-    // //     "mettime: {:?} ({})",
-    // //     chrono::Utc.timestamp_opt(wooper.mettime, 0).unwrap(),
-    // //     wooper.mettime
-    // // );
-    // // println!(
-    // //     "pokeball: {}",
-    // //     wooper.pokeball.unwrap_or(Pokeball::Pokeball)
-    // // );
+
     // println!(
-    //     "helditem: {}",
-    //     wooper.helditem.unwrap_or(Item::ConnectionWire)
+    //     "Translated string (code_name): {}",
+    //     TranslationKey::new("code_name").convert_to_string()
     // );
     // println!(
-    //     "status: {}",
-    //     wooper.condition.unwrap_or(StatusCondition::BadlyPoison)
+    //     "Translated string (name): {}",
+    //     TranslationKey::new("name").convert_to_string()
     // );
-    // println!("pokerus: {}", wooper.pokerus);
-    // println!("nature: {}", wooper.nature);
-    // println!("shiny: {}", wooper.shiny);
-    // println!("ability: {}", wooper.ability);
+    // println!(
+    //     "Translated string (pokemon/bulbasaur/species): {}",
+    //     Pokemon::Bulbasaur.get_base().name.convert_to_string()
+    // );
+    // println!("Translated string (type/fire): {}", Type::Fire);
+    // println!("Translated string (gender/male): {}", Gender::Male);
+    // println!(
+    //     "Translated string (move/tackle/desc): {}",
+    //     Move::Tackle.get_base().desc.convert_to_string()
+    // );
+    // println!("Translated string (nature/quirky): {}", Nature::Quirky);
 
-    // println!("evolevel: {}", wooper.base.get_evolution_level().unwrap());
+    let plr = Player {
+        trainer: Trainer {
+            info: OTInformation {
+                sid: generate_trainer_id(),
+                id: generate_trainer_id(),
+                lang: Language::English,
+                gender: Gender::Male,
+                name: "Yes",
+            },
+        },
+        pc: PC::new_dummy(),
+        money: 0,
+        party: [None, None, None, None, None, None],
+    };
 
-    // println!("moves: ");
-    // for item in wooper.moves.into_iter().enumerate() {
-    //     if item.1.is_some() {
-    //         let move_data = item.1.as_ref().unwrap();
-    //         println!(
-    //             "   {} : {}/{}",
-    //             move_data.base.get_base().name.convert_to_string(),
-    //             move_data.pp,
-    //             move_data.get_max_pp()
-    //         );
-    //         println!(
-    //             "      --> {}",
-    //             move_data.base.get_base().desc.convert_to_string()
-    //         )
-    //     } else {
-    //         println!("   -");
-    //     }
-    // }
+    let wooper = generate_wild_pokemon(Pokemon::Ivysaur, 69, &plr);
 
-    std::thread::sleep(std::time::Duration::from_secs(100));
+    println!("health evs: {}", wooper.get_ev(&Stat::Health));
+    println!("health ivs: {}", wooper.get_iv(&Stat::Health));
+    println!("nature: {}", wooper.nature);
+    println!("health stat: {}", wooper.calculate_stat(&Stat::Health));
+    println!("lvl {} ({} exp)", wooper.get_level(), wooper.exp,);
+    println!(
+        "Personality: 0x{} (0b{})",
+        hex::decimal_to_hex(wooper.pid),
+        hex::decimal_to_binary(wooper.pid)
+    );
+    // println!(
+    //     "mettime: {:?} ({})",
+    //     chrono::Utc.timestamp_opt(wooper.mettime, 0).unwrap(),
+    //     wooper.mettime
+    // );
+    // println!(
+    //     "pokeball: {}",
+    //     wooper.pokeball.unwrap_or(Pokeball::Pokeball)
+    // );
+    println!("helditem: {}", wooper.helditem.unwrap_or(Item::LuckyEgg));
+    println!(
+        "status: {}",
+        wooper.condition.unwrap_or(StatusCondition::BadlyPoison)
+    );
+    println!("pokerus: {}", wooper.pokerus);
+    println!("nature: {}", wooper.nature);
+    println!("shiny: {}", wooper.shiny);
+    println!("ability: {}", wooper.ability);
+
+    println!("evolevel: {}", wooper.base.get_evolution_level().unwrap());
+
+    println!("moves: ");
+    for item in wooper.moves.into_iter().enumerate() {
+        if item.1.is_some() {
+            let move_data = item.1.as_ref().unwrap();
+            println!(
+                "   {} : {}/{}",
+                move_data.base.get_base().name.convert_to_string(),
+                move_data.pp,
+                move_data.get_max_pp()
+            );
+            println!(
+                "      --> {}",
+                move_data.base.get_base().desc.convert_to_string()
+            )
+        } else {
+            println!("   -");
+        }
+    }
 }
