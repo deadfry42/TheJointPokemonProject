495723c4a600d3dc3b9987dca665933bca88adf6
deadfry42 <pooface442@gmail.com>
2025-06-19 21:46:40 +0100

    try to bundle sdl with the exec on mac and linux

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // rust-sdl2 bundled
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path");
+
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
+}
