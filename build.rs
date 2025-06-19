1ed934a409f073026eb8b16060dab675d21d7c34
deadfry42 <pooface442@gmail.com>
2025-06-19 22:01:53 +0100

    i think i read the docs incorrectly

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // rust-sdl2 bundled
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path");
-
-    #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
-}
